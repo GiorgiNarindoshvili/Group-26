291c1bde4fcb1c0fc6f31ff62207f592843557e7
GiorgiNarindoshvili <giorgig20099@gmail.com>
2025-03-30 00:51:19 -0400

    commit

--- Level 82/Classwork/py.py
+++ Level 82/Classwork/py.py
@@ -28,3 +28,64 @@
 #4 Never visit a . . . !?
 # def subtract_sum(n):
 #     return 'apple'
+
+#5) https://www.codewars.com/kata/67b7a527c9f842fd3b02adb8/train/python
+# speed of aircrafts is given in *knots*
+# travelTime is in *minutes*
+# travel distance should be returned in *kilometers*
+
+# def travel_distance(avg_speed, travel_time):
+#     travel_hours = travel_time / 60
+#     travel_miles = avg_speed * travel_hours
+#     travel_kms = travel_miles * 1.852
+#     return round(travel_kms, 2)
+
+#6) https://www.codewars.com/kata/5a34b80155519e1a00000009/train/python
+# def multiple_of_index(arr):
+#     res = []
+#     for i in range(len(arr)):
+#         if arr[i] == 0 or (i != 0 and arr[i] % i == 0):
+#             res.append(arr[i])
+#     return res
+
+#7) https://www.codewars.com/kata/586ee462d0982081bf001f07/train/python
+# def fillable(stock, merch, n):
+#     return stock.get(merch,0) >= n
+
+#8) https://www.codewars.com/kata/58e43389acfd3e81d5000a88/train/python
+# def circle_circumference(circle):
+#     return 2 * circle.radius * 3.141592653589793238
+
+#9) https://www.codewars.com/kata/59c8b38423dacc7d95000008/train/python
+# def is_valid(formula):
+#     if 1 in formula and 2 in formula:
+#         return False
+#     elif 3 in formula and 4 in formula:
+#         return False
+#     elif (5 in formula and 6 not in formula) or (5 not in formula and 6 in formula):
+#         return False
+#     elif 7 in formula or 8 in formula:
+#         return True
+#     return False
+
+#10) https://www.codewars.com/kata/5866fc43395d9138a7000006/train/python
+# def ensure_question(s):
+#     if s.endswith("?"):
+#         return s
+#     return s + "?"
+
+#11) https://www.codewars.com/kata/57b58827d2a31c57720012e8/train/python
+# def fuel_price(litres, ppl):
+#     discountCents = 0
+#     if (litres >= 2):
+#         discountCents = 0.05
+#     if (litres >= 4):
+#         discountCents = 0.1
+#     if (litres >= 6):
+#         discountCents = 0.15
+#     if (litres >= 8):
+#         discountCents = 0.2
+#     if (litres >= 10):
+#         discountCents = 0.25
+#     total = litres * ppl - discountCents * litres
+#     return round(total, 2)
---
+++ Level 82/Homework/py.py
@@ -0,0 +1,109 @@
+#1) https://www.codewars.com/kata/6707688c0f597511f6649270/train/python
+# def was_package_received_yesterday(tfrom, tto, start, duration):
+#     sttime = start - tfrom
+#     delivery = sttime + duration
+#     dvr = delivery + tto
+#     return dvr < 0
+
+#2) https://www.codewars.com/kata/65128732b5aff40032a3d8f0/train/python
+# def neutralise(s1, s2):
+#     moq = []
+#     for i in range(len(s1)):
+#         if(s1[i] == "+" and s2[i] == "+"):
+#             moq.append("+")
+#         elif(s1[i] == "-" and s2[i] == "-"):
+#             moq.append("-")
+#         else:
+#             moq.append("0")
+#     return "".join(moq) 
+
+#3) https://www.codewars.com/kata/521cd52e790405a74800032c/train/python
+# def wrap(value):
+#     return {"value":value}
+
+#4) https://www.codewars.com/kata/545b342082e55dc9da000051/train/python
+# def partition(arr, cla):
+#     l1, l2 = [], []
+#     for i in arr:
+#         if cla(i):
+#             l1.append(i)
+#         else:
+#             l2.append(i)
+#     return l1, l2
+
+#5) https://www.codewars.com/kata/54dba07f03e88a4cec000caf/train/python
+# class Dog():
+#     def __init__(self, breed):
+#         self.breed = breed
+#     def bark(self):
+#         return "Woof"
+    
+        
+# snoopy = Dog("Beagle")
+
+# snoopy.bark = lambda: "Woof"
+
+# scoobydoo = Dog("Woof")
+
+#6) https://www.codewars.com/kata/55ca43fb05c5f2f97f0000fd/train/python
+# def list_animals(animals):
+#     lst = ""
+#     for i in range(len(animals)):
+#         lst += str(i + 1) + '. ' + animals[i] + '\n'
+#     return lst
+
+#7) https://www.codewars.com/kata/56bc1acf66a2abc891000561/train/python
+# from preloaded import greek_alphabet
+
+
+# def greek_comparator(lhs, rhs):
+#     lhsi = greek_alphabet.index(lhs)
+#     rhsi = greek_alphabet.index(rhs)
+    
+#     if lhsi < rhsi:
+#         return -1
+#     elif lhsi > rhsi:
+#         return 1
+#     else: 
+#         return 0
+
+#8) https://www.codewars.com/kata/568018a64f35f0c613000054/train/python
+# class Guesser:
+#     def __init__(self, number, lives):
+#         self.number = number
+#         self.lives = lives
+#     def guess(self,n):
+#         if self.lives == 0:
+#             raise Except("Omae wa mo shindeiru")
+#         elif n  == self.number:
+#             return True
+#         self.lives -= 1
+#         return False
+
+#9) https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b/train/python
+# def sakura_fall(v):
+#     if v <= 0:
+#         return 0
+#     return 400 / v
+
+#10) https://www.codewars.com/kata/577a6e90d48e51c55e000217/train/python
+# def hotpo(ne):
+#     if ne < 2:
+#         return 0
+#     return hotpo(3 * ne + 1 if ne % 2 else ne / 2) + 1
+
+#11)https://www.codewars.com/kata/5786f8404c4709148f0006bf/train/python
+# def starting_mark(height):
+#     return round(height *  3.93484 + 3.4691,2)
+
+#12) https://www.codewars.com/kata/5763bb0af716cad8fb000580/train/python
+# def count_squares(cuts):
+#     return (cuts + 1) ** 3 - (cuts - 1) ** 3
+
+#13) https://www.codewars.com/kata/575fa9afee048b293e000287/train/python
+# def how_much_water(water, load, clothes):
+#     if clothes > 2 * load:
+#         return 'Too much clothes'
+#     elif clothes < load:
+#         return 'Not enough clothes'
+#     return round(water * 1.1 ** (clothes - load),2) 
