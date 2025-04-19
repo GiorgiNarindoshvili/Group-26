f8fa1a1aa29f552a1ce584cac8fa96859e0ca5a2
GiorgiNarindoshvili <giorgig20099@gmail.com>
2025-04-19 00:07:41 -0400

    commit

--- Level 89/Homework/index.js
+++ Level 89/Homework/index.js
@@ -1,9 +1,9 @@
 fetch("https://api.adviceslip.com/advice")
-    .then(res => res.json())
-    .then(data => {
-    const advice1= document.getElementById('advi');
-    const advice = data.slip;
-    advice1.innerHTML = `
+    .then(response => response.json())
+    .then(adciv => {
+    const advicee= document.getElementById('advi');
+    const advice = adciv.slip;
+    advicee.innerHTML = `
         <p>ADVICE #${advice.id}</p>
         <h1>${advice.advice}</h4>
         <img src='./images/pattern-divider-desktop.svg' id="img1">
