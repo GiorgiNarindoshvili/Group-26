b1dec6109b369026167acd88ae8814ed221bc3ee
GiorgiNarindoshvili <giorgig20099@gmail.com>
2025-05-21 19:10:46 -0400

    commit

---
+++ Level 98/Classwork/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+    const num1 = 5;
+    const num2 = 5;
+    const answer = num1 + num2
+
+    return(
+      <div>
+        <h1>${num1} + {num2} = {answer}</h1>
+      </div>
+    )
+}
+
+export default App
---
+++ Level 98/Homework/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  const namee1 = "Giorgi"
+  return(
+    <div>
+      <h1>Hello {namee1}</h1>
+    </div>
+  )
+}
+
+export default App
