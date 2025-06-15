f6c2025fda522d1da1648a339c2a690d43bbcd26
GiorgiNarindoshvili <giorgig20099@gmail.com>
2025-06-14 23:21:34 -0400

    commit

--- Level 106/Classwork/src/App.jsx
+++ Level 106/Classwork/src/App.jsx
@@ -1,7 +1,7 @@
 //1 What is a JSX?
 //JSX stands for javascript XML and we can write html code into a jsx for more faster websites and it makes HTML code more easier to read and write
 //2 How virtual DOM works?
-//as we know, When code repeats multiple times, it crashes the website and website gets stucked, according to that error, website stops working and customers are so unsatisfied, but in Virtual DOM, code doesn't repeat multiple times, so it makes website more faster and comfortable for customers, when something bugs on the web, virtual DOM works only on that bug, other components are working properly which is subject that says that jsx is better than default vanilla javascript
+//as we know, When code repeats multiple times, it crashes the website and website gets stucked, according to that error, website stops working and customers are so unsatisfied, but in Virtual DOM, code doesn't repeat multiple times, so it makes website more faster and comfortable for customers, when something bugs on the web, virtual DOM works only on that bug, how it fixes only that bug? Virtual DOM has two copies one old, one new, When something changes on new VDOM, it compares new one to old, then new VDOM sends a request in original DOM where it determines what'd changed in new VDOM, based on that other components are working properly which is subject that says that jsx is better than default vanilla javascript
 
 
 
